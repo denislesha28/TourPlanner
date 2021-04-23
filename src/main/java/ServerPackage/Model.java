95543da7fd7e085af64477e182ca5c66696ad220
Denis Lesha <leshadenis@gmail.com>
2021-04-23 18:46:04 +0200

    Generating Random Tour with 1e+46 possibilites

--- src/main/java/ServerPackage/Model.java
+++ src/main/java/ServerPackage/Model.java
@@ -45,17 +45,16 @@ public class Model {
     }
 
     private String generateRandomString(){
-        int leftLimit = 97; // letter 'a'
+        int leftLimit = 48; // numeral '0'
         int rightLimit = 122; // letter 'z'
         int targetStringLength = 10;
         Random random = new Random();
-        StringBuilder buffer = new StringBuilder(targetStringLength);
-        for (int i = 0; i < targetStringLength; i++) {
-            int randomLimitedInt = leftLimit + (int)
-                    (random.nextFloat() * (rightLimit - leftLimit + 1));
-            buffer.append((char) randomLimitedInt);
-        }
-        String generatedString = buffer.toString();
+
+        String generatedString = random.ints(leftLimit, rightLimit + 1)
+                .filter(i -> (i <= 57 || i >= 65) && (i <= 90 || i >= 97))
+                .limit(targetStringLength)
+                .collect(StringBuilder::new, StringBuilder::appendCodePoint, StringBuilder::append)
+                .toString();
 
         return generatedString;
     }
